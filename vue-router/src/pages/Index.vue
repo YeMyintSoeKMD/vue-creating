ee09825f6c4776aff036dab409932aee605e7ec8
yemyintsoe <yms.yemyintsoe@gmail.com>
2023-11-25 21:50:46 +0630

    yms

---
+++ vue-router/src/pages/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        <h3>Index Page</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti unde ipsum laborum illum, asperiores, velit optio in ullam blanditiis aut temporibus maxime facere cupiditate aspernatur id nulla voluptas repudiandae. Culpa.</p>
+    </div>
+</template>
+<script setup>
+</script>
+<style>
+    
+</style>
